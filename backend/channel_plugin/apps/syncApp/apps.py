f840a8f7bc173a322a6239e001944f6ecdf525ec
TobeTek <katchyemma@gmail.com>
2021-10-13 22:18:28 +0100

    Created Task Scheduling skeleton : syncApp

--- backend/channel_plugin/apps/syncApp/apps.py
+++ backend/channel_plugin/apps/syncApp/apps.py
@@ -1,5 +1,22 @@
+from datetime import datetime,timedelta
+from pytz import utc
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.memory import MemoryJobStore
+
+
+scheduler = BackgroundScheduler()
+def job_function():
+    print(datetime.now().time().strftime('%H:%M:%S'))
+
+
 from django.apps import AppConfig
 
 
 class SyncAppConfig(AppConfig):
     name = 'apps.syncApp'
+
+    def ready(self):
+        print("The Alpha Dog is here")
+        scheduler.add_job(job_function, "interval", seconds=5)
+        scheduler.start()
+
